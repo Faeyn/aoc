4ac2a1e2f87cf508f5c88ab66f96852cb75f4194
Hilfon Tang <hilfon.tang@lely.com>
2025-12-06 13:29:26 +0100

    refactor

--- src/2025/day06.py
+++ src/2025/day06.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import re
 import operator as op
-from collections import defaultdict
 
 with open(Path(__file__).parent / '.input/day06_input') as f:
     data = f.read()
@@ -14,49 +13,33 @@ with open(Path(__file__).parent / '.input/day06_input') as f:
 
 data = data.splitlines()
 ops = {'+': op.add, '*': op.mul}
-
-def get_nums(s: str) -> list[str]:
-    return [int(x) for x in re.findall(r'-?\d+', s)]
-
-def get_op(s: str) -> list[str]:
-    return [ops[str(x)] for x in re.findall(r'[+*]', s)]
+input_op = [ops[str(x)] for x in re.findall(r'[+*]', data[-1])]
 
 input = []
 for line in data[:-1]:
-    input.append(get_nums(line))
+    input.append([int(x) for x in re.findall(r'-?\d+', line)])
 
-input_op = get_op(data[-1])
-part_1 = 0
+input1 = list(zip(*input))
 
-for i, op_c in enumerate(input_op):
-    ans = 1 if op_c == op.mul else 0
-    for row in input:
-        ans = op_c(ans, row[i])
-    part_1 += ans
+def homework(inp) -> int:
+    tot = 0
+    for op_c, vals in zip(input_op, inp):
+        ans = 1 if op_c == op.mul else 0
+        for val in vals:
+            ans = op_c(ans, val)
+        tot += ans
+    return tot
 
-print(part_1)
+print("Part1: ", homework(input1))
 
-input2 = [defaultdict(str) for _ in range(len(input_op))]
-for line in data[:-1]:
-    q = 0
-    v_p = "." 
-    for i, v in enumerate(line):
-        if v != " ":
-            input2[q][i] += str(v)
-        else:
-            if v_p != " " and v_p != ".":
-                q += 1
+input2 = [[] for _ in range(len(input_op))]
 
-        v_p = v
-
-part_2 = 0
-for i_c, d in enumerate(input2):
-    ans = 1 if input_op[i_c] == op.mul else 0
-    for v in d.values():
-        ans = input_op[i_c](ans, int(v))
-    part_2 += ans
-
-print(part_2)
+q = 0
+for i_c in range(len(data[0])):
+    if all([line[i_c] == " " for line in data[:-1]]):
+        q += 1
+        continue
+    input2[q].append(int("".join([line[i_c] for line in data[:-1] if line[i_c] != " "])))
 
 
-
+print("Part2: ", homework(input2))
