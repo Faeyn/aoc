8fe5fb58a9f0a796040808e759a7290e5cd6f448
Hilfon Tang <hilfon.tang@lely.com>
2025-12-06 13:48:10 +0100

    reorder day6

--- src/2025/day06.py
+++ src/2025/day06.py
@@ -5,21 +5,27 @@ import operator as op
 with open(Path(__file__).parent / '.input/day06_input') as f:
     data = f.read()
 
-# data = """123 328  51 64 
-#  45 64  387 23 
-#   6 98  215 314
-# *   +   *   +
-# """
+data = """123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +
+"""
 
 data = data.splitlines()
-ops = {'+': op.add, '*': op.mul}
-input_op = [ops[str(x)] for x in re.findall(r'[+*]', data[-1])]
+input_op = [op.add if x == '+' else op.mul for x in re.findall(r'[+*]', data[-1])]
 
-input = []
+input1 = []
 for line in data[:-1]:
-    input.append([int(x) for x in re.findall(r'-?\d+', line)])
+    input1.append([int(x) for x in re.findall(r'-?\d+', line)])
+input1 = list(zip(*input1))
 
-input1 = list(zip(*input))
+q = 0
+input2 = [[] for _ in range(len(input_op))]
+for col in zip(*data[:-1]):
+    if all([v == " " for v in col]):
+        q += 1
+        continue
+    input2[q].append(int("".join(col)))
 
 def homework(inp) -> int:
     tot = 0
@@ -31,15 +37,4 @@ def homework(inp) -> int:
     return tot
 
 print("Part1: ", homework(input1))
-
-input2 = [[] for _ in range(len(input_op))]
-
-q = 0
-for i_c in range(len(data[0])):
-    if all([line[i_c] == " " for line in data[:-1]]):
-        q += 1
-        continue
-    input2[q].append(int("".join([line[i_c] for line in data[:-1] if line[i_c] != " "])))
-
-
 print("Part2: ", homework(input2))
