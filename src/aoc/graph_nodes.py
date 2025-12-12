b06e8974d6aaef519f7e1319eb1198c1c25daf0b
Hilfon Tang <hilfon.tang@lely.com>
2025-12-12 13:11:02 +0100

    day12

--- src/aoc/graph_nodes.py
+++ src/aoc/graph_nodes.py
@@ -12,4 +12,4 @@ def grapher(nodes, edges):
         graph.edge(*edge)
 
     # Save the graph visualization to a file (e.g., in PNG format)
-    graph.render('output', format='png', cleanup=True)
+    graph.render('output', format='png', view=True, cleanup=True)
